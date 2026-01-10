d639e037dc1af684b13f3752906f4d56e3b20603
Hannah Plowes <hannahplowes@proton.me>
2026-01-10 13:25:57 +1100

    nvim shtuff

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,29 +1,66 @@
--- my nvim config yippee :3
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
+end
+vim.opt.rtp:prepend(lazypath)
 
-local vim = vim -- suppress lsp warnings
-local o = vim.opt
-o.showmatch = true
-o.ignorecase = true
-o.smartcase = true
-o.swapfile = false
-o.hlsearch = true
-o.incsearch = true
-o.tabstop = 4
-o.softtabstop = 4
-o.shiftwidth = 4
-o.autoindent = true
-o.expandtab = true
-o.number = true
-o.relativenumber = true
-o.ttyfast = true
-o.mouse = "a"
-o.wildmode = { "longest", "list" }
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
-local g = vim.g
-g.mapleader = ","
-g.maplocalleader = ","
+vim.opt.showmatch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.swapfile = false
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.autoindent = true
+vim.opt.expandtab = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.ttyfast = true
+vim.opt.mouse = 'a'
+vim.opt.wildmode = { 'longest', 'list' }
 
-vim.cmd.highlight({ "NonText", "guibg=NONE ctermbg=NONE" })
-vim.cmd.highlight({ "Normal", "guibg=NONE ctermbg=NONE" })
-vim.cmd.highlight({ "NormalNC", "guibg=NONE ctermbg=NONE" })
-vim.cmd.highlight({ "SignColumn", "guibg=NONE ctermfg=NONE ctermbg=NONE" })
+require('lazy').setup({
+  defaults = { lazy = false, version = false, },
+  checker = { enabled = true, notify = false, },
+  { -- everforest
+    'neanias/everforest-nvim',
+    priority = 1000,
+    config = function()
+      require('everforest').setup({
+        background = 'hard',
+        transparent_background_level = 1,
+      })
+      require('everforest').load()
+    end,
+  },
+  { -- lualine
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('lualine').setup()
+    end,
+  },
+  { -- telescope
+    'nvim-telescope/telescope.nvim',
+    requires = {
+      'nvim-lua/plenary.nvim',
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
+    config = function()
+      require('telescope').setup({})
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+    end,
+  }
+})
+
+
